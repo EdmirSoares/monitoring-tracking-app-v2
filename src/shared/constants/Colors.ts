e9c1e9fd0ab9626b4f2fb0b6c26e1ce78bee5052
EdmirSoares <dimisoares10@gmail.com>
2025-09-12 16:32:54 -0300

    feat: add color constants and store keys for application

---
+++ src/shared/constants/Colors.ts
@@ -0,0 +1,21 @@
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
+
+export default {
+    light: {
+        text: "#000",
+        buttonText: "#fff",
+        background: "#f5f5f5ff",
+        tint: tintColorLight,
+        tabIconDefault: "#ccc",
+        tabIconSelected: tintColorLight,
+    },
+    dark: {
+        text: "#fff",
+        buttonText: "#fff",
+        background: "#000",
+        tint: tintColorDark,
+        tabIconDefault: "#ccc",
+        tabIconSelected: tintColorDark,
+    },
+};
---
+++ src/shared/constants/StoreKeys.ts
@@ -0,0 +1,6 @@
+export const STORE_KEYS = {
+    TOKEN: "auth.token",
+    TIMESTAMP: "auth.timestamp",
+    USERID: "auth.id_user",
+    USER_NAME: "auth.user_name",
+};
